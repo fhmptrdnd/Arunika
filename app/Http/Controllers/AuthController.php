ce97a2b96e6c3b6428e6b1f674a240f70310a2c8
Allyya Novita <aliyaliyaa00@gmail.com>
2026-03-24 04:59:21 +0700

    redirect role guru

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -156,6 +156,8 @@ class AuthController extends Controller
             $role = Auth::user()->role;
             if ($role === 'admin') {
                 return redirect()->intended('/admin/dashboard');
+            } elseif ($role === 'guru') {
+                return redirect()->intended('/guru/dashboard');
             } elseif ($role === 'parent') {
                 return redirect()->intended('/orangtua/profil');
             } else {
